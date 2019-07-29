cdbd98027e9d1299cbe1cb5bb03f666aa43d0905
Your Name <you@example.com>
2019-07-29 12:03:12 -0500

    Added Validations

--- apps/semi_app/models.py
+++ apps/semi_app/models.py
@@ -1,5 +1,25 @@
+from __future__ import unicode_literals
 from django.db import models
 
+36
+class ShowManager(models.Manager):
+
+    def basic_validator(self, postData):
+            errors = {}
+            # add keys and values to errors dictionary for each invalid field
+            if len(postData['title']) < 3:
+                print("not long enough")
+                errors["title"] = "Show name should be at least 1 character"
+            if len(postData['network']) < 1:
+                print("not long enough")
+                errors["network"] = "Network should be at least 1 character"
+            if len(postData['date']) < 1:
+                print("no date entered")
+                errors["date"] = "Must enter Shows Release Date"            
+            if len(postData['description']) < 10:
+                print("not long enough")
+                errors["description"] = "Blog description should be at least 10 characters"
+            return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=45)
@@ -8,4 +28,5 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
 
--- apps/semi_app/views.py
+++ apps/semi_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from .models import Show
+from .models import *
+from django.contrib import messages
 # Create your views here.
 
 def new_show(request):
+
     return render(request,'semi_app/new_show.html')
 
 def all_shows(request):
@@ -27,25 +29,51 @@ def shows_id(request, show_id):
     return render(request,'semi_app/shows_id.html', context) 
 
 def update_show(request, show_id):
-    show = Show.objects.get(id=show_id)
-    show.title = request.POST['title']
-    show.network = request.POST['network']
-    show.description = request.POST['description']
-    show.release_date = request.POST['date']
-    show.save()
-    context ={
-        'show': show
-    }
+    # errors = request
+    errors = Show.objects.basic_validator(request.POST)
+
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        print(errors)
+        print(show_id)
+        return redirect(f'/shows/{show_id}/edit')
+    else:
+        show = Show.objects.get(id=show_id)
+        show.title = request.POST['title']
+        show.network = request.POST['network']
+        show.description = request.POST['description']
+        show.release_date = request.POST['date']
+        show.save()
+        context ={
+            'show': show
+        }
     
     return redirect(f'/shows/{show.id}')
+    # return redirect(f'/shows')
 
 
 
 def shows_create(request):
-    Show.objects.create(title=request.POST['title'],network=request.POST['network'],release_date=request.POST['date'],description=request.POST['description'])
-    print(f'////////CREATE////////')
-    show = Show.objects.last()
-    show.save()
+    # errors = request
+    errors = Show.objects.basic_validator(request.POST)
+
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        print(errors)
+        return redirect('/shows/new')
+    else:
+        Show.objects.create(title=request.POST['title'],network=request.POST['network'],release_date=request.POST['date'],description=request.POST['description'])
+        print(f'////////CREATE////////')
+        show = Show.objects.last()
+        show.save()
     return redirect(f'/shows/{show.id}')
 
 def destroy_show(request, show_id):
